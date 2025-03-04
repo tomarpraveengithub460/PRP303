dbd077078c91217fc24069d5c0b371a41acee89e
Your Name <you@example.com>
2025-03-04 12:37:50 +0530

    Adding Code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
-    res.status(200).send('Hello Ronit! with &#x1F49D;');
+    res.status(200).send('Hello Ronit!');
 })
 
 // Inserting the data
